36984c6fd08ab8aa74c06a5c1a702a4118eb61f7
lucasviniz <howaitosame_7@hotmail.com>
2025-03-22 22:01:28 -0300

    added bottomsheet, search data on api, styling components

---
+++ src/components/category/index.tsx
@@ -0,0 +1,20 @@
+import { Pressable, PressableProps, Text } from "react-native"
+import { s } from "./styles"
+import { categoriesIcons } from "@/utils/categories-icons"
+import { colors } from "@/styles/theme"
+
+type CategoryProps = PressableProps & {
+    iconId: string,
+    isSelected?: boolean,
+    name: string
+}
+
+export function Category ({ name, iconId, isSelected = false, ...rest }: CategoryProps){
+    const Icon = categoriesIcons[iconId]
+    return (
+        <Pressable style={[s.container, isSelected && s.containerSelected]} {...rest}>
+            <Icon size={16} color={colors.gray[isSelected ? 100 : 400]}/>
+            <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
+        </Pressable>
+    )
+}
